bdb3bcf008eb73715a1e6203d1c7d003e6fc32a0
Tisha Dey <tishadey2025@outlook.com>
2026-03-16 22:31:26 +0600

    :green_heart: dynamic blog data fetched

--- src/main.jsx
+++ src/main.jsx
@@ -17,6 +17,7 @@ const router = createBrowserRouter([
       {
         path: "/blogs",
         element: <Blogs />,
+        loader:()=>fetch("https://dev.to/api/articles?per_page=20&top=7")
       },
       {
         path: "/bookmarks",
